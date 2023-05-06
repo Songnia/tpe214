a3bbbade612a293e696f0bf4c0870f66a9640ac2
Wilfried <sogniaw@gmail.com>
2023-05-06 16:44:22 +0200

    Ajout du rapport de projet+somme.c

---
+++ somme.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Somme(a,b)! \n Entrer les valeurs de a et b\n");
+    printf("a: ");
+    scanf("%d",&a);
+    printf("b: ");
+    scanf("%d",&b);
+    printf("Somme(%d,%d)=%d",a,b,a+b);
+    return 0;
+}
